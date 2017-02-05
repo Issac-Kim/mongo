2166d9130f125c3af06e3f7870b33cd30208ae27
Edmond Lam <elam3@stuy.edu>
2017-02-04 20:28:02 -0500

    finished it for you isaac, check to make sure it works

--- db_builder.py
+++ db_builder.py
@@ -7,9 +7,41 @@ server = MongoClient('149.89.150.100')
 
 db = server['pyschic-potato']
 
-students = db.students
+c = db['psychic-potato']['students']
 
 students = students.read().split()
 
+courses = courses.read().split()
 
+courses.pop(0)
+
+students.pop(0)
+
+dictstudents = []
+
+for item in students:
+    student = item.split(',')
+    studentdict = {}
+    studentdict['name'] = student[0]
+    studentdict['age'] = int(student[1])
+    studentdict['id'] = student[2]
+    studentdict['classes'] = {}
+    dictstudents.append(studentdict)
+    print item
+        
+print dictstudents
+
+for element in dictstudents:
+    num = element['id']
+    for items in courses:
+        course = items.split(',')
+        if course[2] != num:
+            print element
+            c.insert_one(element)
+            break
+        else:
+            element['classes'][course[0]]= int(course[1])
+
+
+    
 
